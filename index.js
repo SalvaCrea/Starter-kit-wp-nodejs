525cfb448683a0a1d8ebca2788922ea5cb92cca1
Cardona <hebustha@gmail.com>
2018-01-28 16:03:01 +0100

    ++

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 // module.exports.js
 // ========
-
+var fs        = require('fs');
+var http      = require('https');
 var toolFiles = require('tool-files');
+
 var self = module.exports;
 /**
  * Return Configuration
  * @return {[object]} Configuration object
  */
-self.getConfiguration = function(){
+self.getConfiguration = function() {
     return toolFiles.getFileJson('./configuration.json');
 }
 /**
  * Return Path container theme folder
  * @return {[string]} Path theme folder
  */
-self.getPathFolderTheme = function(){
+self.getPathFolderTheme = function() {
     return './wordpress/wp-content/themes';
 }
 /**
  * Return Path container theme folder
  * @return {[string]} Path theme folder
  */
-self.getPathTheme = function(){
+self.getPathTheme = function() {
     return self.getPathFolderTheme() + '/' + self.getConfiguration().themeName;
+}
+/**
+ * Dowload and write file in the destination
+ * @param {[string]} url  The url of file
+ * @param {[string]} dest The path of destination
+ * @param {Function} cb   Function callback
+ */
+self.dowloadFile = function(url, dest, cb = ()) {
+    var file = fs.createWriteStream(dest);
+    var request = http.get(url, function(response) {
+        response.pipe(file);
+        file.on('finish', function() {
+            file.close(cb);  // close() is async, call cb after close completes.
+        });
+    }).on('error', function(err) { // Handle errors
+        fs.unlink(dest); // Delete the file async. (But we don't check the result)
+        if (cb) cb(err.message);
+    });
 }
